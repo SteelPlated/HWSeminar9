14732dc0eecc0151d9034fddd12f519942ff22fd
SteelPlated <mix.singatulin@yandex.ru>
2022-06-11 13:45:25 +0500

    1

---
+++ HWSeminar9/task64/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int count = 2;
+
+void PrintNum(int n, int count)
+{
+  if (count > n) return;
+  PrintNum(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+PrintNum(n, count);
+Console.Write(1);
---
+++ HWSeminar9/task68/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int functionAkkererman = Akker(m, n);
+
+int Akker(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Akker(m - 1, 1);
+  else return Akker(m - 1, Akker(m, n - 1));
+}
+Console.Write($"Функция Аккермана = {functionAkkererman} ");
